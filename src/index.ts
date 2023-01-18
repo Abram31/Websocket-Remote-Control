2f9f5e6d59f9234cd1169133033eef0ef269b273
Abram31 <a447623993@gmail.com>
2023-01-18 19:44:20 +0100

    feat: add nut-tree and test for its

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 import { WebSocketServer } from 'ws';
+import {
+  mouse,
+  screen,
+  singleWord,
+  sleep,
+  useConsoleLogger,
+  ConsoleLogLevel,
+  straightTo,
+  centerOf,
+  Button,
+  getActiveWindow,
+  right,
+} from '@nut-tree/nut-js';
+import { randomInt } from 'crypto';
+
 const port = 8080;
 const ws = new WebSocketServer({ port: port });
 ws.on('open', () => {
@@ -6,7 +21,8 @@ ws.on('open', () => {
 });
 ws.on('connection', (ws) => {
   ws.on('message', (data) => {
-    console.log(data);
+    console.log(data.toString());
+    mouse.move(right(50));
   });
   console.log('new connection');
   ws.send('Hello client');
